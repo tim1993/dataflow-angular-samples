b9f5563159bd4d69b603f9d4da4ffeb4f99eb78a
Dominik Deschner <deschner@medialesson.de>
2022-05-06 19:17:34 +0200

    Adds login

--- examples/no-state-management/PumpItStudios-NoState/projects/pumpit-facade/src/app/facades/application.facade.ts
+++ examples/no-state-management/PumpItStudios-NoState/projects/pumpit-facade/src/app/facades/application.facade.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { IUser } from 'projects/base-components/src/lib/models/user.model';
+import { UserService } from 'projects/base-components/src/lib/user.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApplicationFacade {
-  constructor() {}
+  private userSubject = new BehaviorSubject<IUser | null>(null);
+
+  public user$ = this.userSubject.asObservable();
+
+  constructor(private userService: UserService) {}
+
+  public login(user: string, password: string) {
+    this.userService
+      .authenticate(user, password)
+      .subscribe(this.userSubject.next);
+  }
 }
